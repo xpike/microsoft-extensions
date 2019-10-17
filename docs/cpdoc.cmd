16517a873fcf52b866806a3d2db3d200039eb723
Jason Penniman <jpenniman@gmail.com>
2019-10-16 22:01:43 -0400

    Moved articles to main documentation repo, so only API docs are built here.

--- docs/cpdoc.cmd
+++
@@ -1,4 +0,0 @@
-docfx metadata
-xcopy *.md ..\..\documentation\docfx_project\api\extensions\ /s/e/y
-xcopy *.yml ..\..\documentation\docfx_project\api\extensions\ /s/e/y
-xcopy *.manifest ..\..\documentation\docfx_project\api\extensions\ /s/e/y
